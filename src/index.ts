cf6854c9d70a043ec21cbf36c08c6d5b88817164
Danishsjjd <danishsjjad@gmail.com>
2022-10-25 07:13:59 +0500

    10 unknow & 11 never type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-const phone = <HTMLInputElement | null>document.getElementById("phone");
+function displayEvents(event: unknown) {
+  if (typeof event === "string") event.length;
+  event.show();
+  event.override();
+}
 
-phone?.value;
+function reject(msg: string): never {
+  throw new Error(msg);
+}
+
+//....
+console.log("100 lines of code");
+
+reject("rejected");
+
+console.log("un reachable code");
