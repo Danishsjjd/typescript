2c48573b82e4ae7ead325facce178a43caf0aac0
Danishsjjd <danishsjjad@gmail.com>
2022-10-25 07:06:31 +0500

    9 type assertions

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-type Customer = {
-  birthday: Date;
-};
+const phone = <HTMLInputElement | null>document.getElementById("phone");
 
-function getCustomer(id: number): Customer | null {
-  return id === 0 ? null : { birthday: new Date() };
-}
-
-const customer = getCustomer(1);
-
-customer?.birthday.getFullYear();
-
-// optional chaining
-// optional access element
-// optional call
+phone?.value;
