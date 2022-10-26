f67d22f79dee0c44778d698f2572b3c94476a90f
Danishsjjd <danishsjjad@gmail.com>
2022-10-26 12:09:22 +0500

    exercise oops

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,49 @@
-interface Calender {
-  name: string;
-  addEvent(): void;
-  removeEvent(): void;
-}
+// 1
+class Logger {
+  constructor(public logFile: string) {}
 
-interface CloudCalender extends Calender {
-  sync(): void;
-}
-
-class GoogleCalender implements Calender {
-  constructor(public name: string) {}
-
-  addEvent(): void {
-    throw new Error("Method not implemented.");
-  }
-  removeEvent(): void {
-    throw new Error("Method not implemented.");
+  log(message: string): void {
+    console.log(`writing ${message} to ${this.logFile} file`);
   }
 }
 
-type Person = {
+// 2
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
+  public get fullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+class Employee extends Person {
+  constructor(public salary: number, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
+}
+
+// 3
+// ? What is the difference between private and protected members?
+// * private are not inherited by child classes
+
+// 4
+interface Address {
+  street: string;
+  city: string;
+  zipCode: number;
+}
+
+interface EmployeeInterface {
   name: string;
-  walk(): void;
+  salary: number;
+  address: Address;
+}
+
+let employee: EmployeeInterface = {
+  name: "John Smith",
+  salary: 50_000,
+  address: {
+    street: "Flinders st",
+    city: "Melbourne",
+    zipCode: 3144,
+  },
 };
-
-// type is not extendable
-
-class Students implements Person {
-  constructor(public name: string) {}
-
-  walk(): void {
-    throw new Error("Method not implemented.");
-  }
-}
