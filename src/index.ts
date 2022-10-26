d8dfa2a51c3c18183d5e9def42a57c57371fdab9
Danishsjjd <danishsjjad@gmail.com>
2022-10-26 08:26:50 +0500

    6 assess control keyword

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 class Account {
   readonly id: number;
-  owner: string;
-  balance: number;
+  name: string;
+  private _balance: number;
   nickname?: string;
 
   constructor(id: number, owner: string, balance: number) {
-    this.owner = owner;
+    this.name = owner;
     this.id = id;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error("Invalid amount");
-    this.balance = amount;
+    // record transactions
+    this._balance = amount;
+    this.transactions(amount);
+  }
+
+  private transactions(deposit: number) {
+    console.log(deposit);
+  }
+
+  get balance(): number {
+    return this._balance;
   }
 }
 
 const account = new Account(1, "Danish", 0);
 
 account.deposit(100);
+
+console.log(account.balance);
