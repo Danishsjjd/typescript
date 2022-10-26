037f1a29ad746dbcd1da4929e2eb86fa82a74498
Danishsjjd <danishsjjad@gmail.com>
2022-10-26 12:48:32 +0500

    4 generic functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-// class 2
-// * the problem generic is solving is to set type during declaration so we don't have to repeat class or use union operator
-
-// class 3
-class KeyValuePair<K, V> {
-  constructor(public key: K, public value: V) {}
+function wrapInArray<T>(value: T): T[] {
+  return [value];
 }
-// most of the time we don't have to explicitly write generic types
-const pair = new KeyValuePair<number, string>(1, "2");
+
+const numbers = wrapInArray<string>("1");
+
+class ArrayUtils {
+  public static wrapInArray<T>(value: T): T[] {
+    return [value];
+  }
+}
+
+const numbers1 = ArrayUtils.wrapInArray(3);
