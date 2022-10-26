d7a77bcc9dd026c5eabd9bffb0e36f464483ccdb
Danishsjjd <danishsjjad@gmail.com>
2022-10-26 07:59:47 +0500

    oops 3 creating classes

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,16 @@
-// 1
+class Account {
+  id: number;
+  owner: string;
+  balance: number;
 
-type Users = {
-  name: string;
-  age: number;
-  occupation?: string;
-};
+  constructor(id: number, owner: string, balance: number) {
+    this.owner = owner;
+    this.id = id;
+    this.balance = balance;
+  }
 
-let users: Users[] = [
-  {
-    name: "John Smith",
-    age: 30,
-    occupation: "Software engineer",
-  },
-  {
-    name: "Kate Müller",
-    age: 28,
-  },
-];
-
-// 2
-
-type Bird = {
-  fly: () => void;
-  canWalk: boolean;
-};
-
-type Fish = {
-  swim: () => void;
-};
-
-type Pet = Bird | Fish;
-
-const fish: Pet = {
-  swim() {},
-};
-
-const bird: Pet = {
-  fly() {},
-  canWalk: true,
-};
-
-// 3
-
-type DaysOfWeek = "Monday" | "Tuesday";
-
-// 4
-// let user = getUser();
-// user?.address?.street
-
-// foo ?? bar();
-
-// 5
-// we don't check the type
-let value: unknown = "a";
-if (typeof value === "string") console.log(value.toUpperCase());
-
-// 6
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
+    this.balance = amount;
+  }
+}
