6713f25d68aa66cd68c4594ee21ef8334c41de02
Danishsjjd <danishsjjad@gmail.com>
2022-10-27 09:34:12 +0500

    3 class decorator

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
-// 1
-function echo<T>(arg: T): T {
-  return arg;
+function Component(constructor: Function): void {
+  console.log("component decorator called");
+  constructor.prototype.ID = Date.now();
+  constructor.prototype.insertInDOM = (): string => {
+    return "Inserting realtime hot module replacement 🔥🔥🔥";
+  };
 }
 
-// 2
-// restrict
-function printName1<T extends { name: string }>(obj: T) {
-  return obj.name;
-}
-
-// 3
-class Entity<T> {
-  constructor(public ID: T) {}
-}
-
-// 4
-// ? Given the following interface, what does keyof User return?
-interface User {
-  userId: number;
-  username: string;
-}
-// * It returns a union of the properties of User: ‘userId’ | ‘username’
+@Component
+class ProfileComponent {}
