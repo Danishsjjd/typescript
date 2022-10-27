023f49ea880daeaa8f2f1744ec173190f51eb07d
Danishsjjd <danishsjjad@gmail.com>
2022-10-27 09:42:22 +0500

    5 decorator composition

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,18 @@ function Component(options: DecoratorOptions) {
   return (constructor: Function) => {
     console.log("component decorator called");
     constructor.prototype.ID = Date.now();
+    constructor.prototype.options = options;
     constructor.prototype.insertInDOM = (): string => {
       return "Inserting realtime hot module replacement 🔥🔥🔥";
     };
   };
 }
 
+function Pipe(constructor: Function): void {
+  console.log("pip constructor called");
+  constructor.prototype.pipe = true;
+}
+
 @Component({ select: "#my-profile" })
+@Pipe
 class ProfileComponent {}
