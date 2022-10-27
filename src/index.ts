9f494301d830b9f3cbad3a58cfb572502eb60a05
Danishsjjd <danishsjjad@gmail.com>
2022-10-27 12:25:33 +0500

    decorators exercise

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,8 @@
-// 9 parameter decorators
-// probably no need to learn
-// if so good to go
-
-interface Account {
-  password: string;
-}
-
-function MinLength(length: number) {
-  return (target: any, propertyName: PropertyKey) => {
-    let value: string;
-
-    const descripter: PropertyDescriptor = {
-      get() {
-        return value;
-      },
-      set(v: string) {
-        if (v.length < length) throw new Error(`Length is less than ${length}`);
-        value = v;
-      },
-    };
-
-    Object.defineProperty(target, propertyName, descripter);
+function Sauce(sauce: string) {
+  return (constructor: Function) => {
+    constructor.prototype.sauce = sauce;
   };
 }
 
-class User implements Account {
-  @MinLength(4)
-  password: string;
-
-  constructor(password: string) {
-    this.password = password;
-  }
-}
-
-const danish = new User("1234");
-danish.password = "12";
-console.log(danish.password);
+@Sauce("pesto")
+class Pizza {}
