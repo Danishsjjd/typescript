f4328edf59dabaedbfdcd67ed9679369987a6e6a
Danishsjjd <danishsjjad@gmail.com>
2022-10-27 11:12:59 +0500

    7 accessor decorators

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 function Log(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
   const original = descriptor.value as Function;
+  console.log("target is", target);
+  console.log("propertyKey is", propertyKey);
   descriptor.value = function (...args: any) {
     console.log("Before");
     original.call(this, ...args);
@@ -7,13 +9,34 @@ function Log(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
   };
 }
 
+function MyCapitalize(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const original = descriptor.get;
+  descriptor.get = function () {
+    const results = original?.call(this);
+    return typeof results === "string" ? results.toUpperCase() : results;
+  };
+}
+
 class ProfileComponent {
+  constructor(public firstName: string, public lastName: string) {}
+
   @Log
   say(msg: string): void {
     console.log("Hello ", msg);
   }
+
+  @MyCapitalize
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
-const profile = new ProfileComponent();
+const profile = new ProfileComponent("danish", "sajjad");
 
 profile.say("World");
+
+console.log(profile.fullName);
